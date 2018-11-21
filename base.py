8fcbf044d0ce62d540beaf24cd4ac13b12bae117
tatikoxlik <tatiana@kozlik.cc>
2018-11-21 17:08:59 +0100

    Dodaję commita

---
+++ base.py
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <title>Dolina niesamowitości</title>
+        <meta charset=UTF-8>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+
+      <nav>
+         <ul>
+           <li><a href="index.html">Strona główna</a></li>
+           <li><a href="about.html">O mnie</a></li>
+           <li><a href="contact.html">Kontakt</a></li>
+           <li><a href="test.html">Test</a></li>
+         </ul>
+       </nav>
+
+      <article>
+Hej,
+jestem autorem tej strony i bardzo lubię serniki. Piec i jeść.
+</article>
+</body>
+<footer>
+  <p>Tatiana</p>
+</footer>
+</html>
